2a4338ca4e313d9f413849a60cec4f9cec3b8c9b
Jatin-4291 <jatin.dua2003@gmail.com>
2025-05-16 10:35:40 +0530

    fixed cors-5

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,5 +1,7 @@
 import io from "socket.io-client";
 
-const socket = io("https://flowboard-1uw3.onrender.com"); // Backend URL
+const socket = io("https://flowboard-1uw3.onrender.com", {
+  withCredentials: true,
+}); // Backend URL
 
 export default socket;
