9a272267115044be7df54fa1cfe2cb53a29463fd
Jatin-4291 <jatin.dua2003@gmail.com>
2025-05-16 10:46:03 +0530

    fixed front url-2

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://flow-board-three.vercel.app/",
+  "https://flow-board-three.vercel.app",
 ];
 app.use(
   cors({
